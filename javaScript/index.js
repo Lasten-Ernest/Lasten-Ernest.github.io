d2e9dcabb4577d7259a2241698a170fc1540a83e
Lasten-Ernest <58563426+Lasten-Ernest@users.noreply.github.com>
2022-06-22 12:00:49 +0200

    Add code to open the menu

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,21 +1,21 @@
-const hamburgerButton = document.querySelector('.menu-button');
+const hamburgerBtn = document.querySelector('.humberger-menu');
 const navButton = document.querySelector('.mobile-nav-menu');
 const exitButton = document.querySelector('.exit-btn');
 const navOptions = document.querySelectorAll('.mobile-nav-link');
 const headSection = document.querySelector('.overlay');
 // Open Menu Bar
-hamburgerButton.addEventListener('click', () => {
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.style.display = ('none');
   navButton.style.display = ('block');
   exitButton.style.display = ('block');
   headSection.style.display = ('block');
-  hamburgerButton.style.display = ('none');
 });
 // Close Menu Bar
 exitButton.addEventListener('click', () => {
   navButton.style.display = ('none');
   exitButton.style.display = ('none');
   headSection.style.display = ('none');
-  hamburgerButton.style.display = ('block');
+  hamburgerBtn.style.display = ('block');
 });
 // Close Menu Bar Using navOptions
 navOptions.forEach((navOption) => {
@@ -23,6 +23,6 @@ navOptions.forEach((navOption) => {
     navButton.style.display = ('none');
     exitButton.style.display = ('none');
     headSection.style.display = ('none');
-    hamburgerButton.style.display = ('block');
+    hamburgerBtn.style.display = ('block');
   });
 });
